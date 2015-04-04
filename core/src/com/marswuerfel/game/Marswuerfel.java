b3c765fe14124928a36c120f1da9d77a6da4966f
Chisao <manuel19972000@yahoo.de>
2015-04-04 18:28:46 +0200

    Revert "Update Marswuerfel.java"
    This reverts commit b920f48b1d9f1341689af8894cecf5ae5af6aa9c.

--- core/src/com/marswuerfel/game/Marswuerfel.java
+++ core/src/com/marswuerfel/game/Marswuerfel.java
@@ -32,7 +32,7 @@ public class Marswuerfel extends ApplicationAdapter {
 	Sound clickUp;
 	
 	Sound cowSound;
-	public static final String TAG = "[MauswÃ¼rfel]";
+	public static final String TAG = "[Mauswürfel]";
 
 	@Override
 	public void create() {
@@ -115,6 +115,7 @@ public class Marswuerfel extends ApplicationAdapter {
 
 		font.draw(batch, "" + tank.getCount(), 667, 400);
 		font.draw(batch, "" + alien.getCount(), 742, 400);
+		font.draw(batch, "Hello Git!", 300, 300);
 		batch.end();
 	}
 	
